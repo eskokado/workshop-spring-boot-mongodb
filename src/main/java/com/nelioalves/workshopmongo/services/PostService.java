91372ab7d571df41694b527e83f4501158a075e8
Edson Shideki Kokado <eskokado@gmail.com>
2018-02-25 09:42:11 -0300

    Consulta simples com query methods

--- src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.nelioalves.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,9 @@ public class PostService {
 		}
 		return post;
 	}
-	
+
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 	
 }
